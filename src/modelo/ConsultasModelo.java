bccc40d3ff24f00f10a1f0a532b729c3428e3182
Alejandro <Duoc UC@B33-17>
2017-12-27 19:37:14 -0300

    Alejandro Contreras  = Inicio de Proyecto // Se termina el modelo y las Consultas en el Modelo

--- src/modelo/ConsultasModelo.java
+++ src/modelo/ConsultasModelo.java
@@ -1,6 +1,9 @@
 package modelo;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.table.DefaultTableModel;
 
 public class ConsultasModelo {
     
@@ -10,10 +13,11 @@ public class ConsultasModelo {
         c= new Conexion();
     }
     
-    public boolean agregarPelicula(Modelo m) {
+    public boolean agregarEmpleado(Modelo m) {
         try {
-            String query = "INSERT INTO empleados(codigo, rut, nombre, apellido, celular, email, sueldo_bruto, est_civil, nom_depto) VALUES(?,?,?,?,?,?,?,?,?)";
+            String query = "INSERT INTO contrata.empleados(codigo, rut, nombre, apellido, celular, email, sueldo_bruto, est_civil, nom_depto) VALUES(?,?,?,?,?,?,?,?,?)";
             PreparedStatement ins = c.getConexion().prepareStatement(query);
+            
             ins.setInt(1, m.getCodigo());
             ins.setString(2, m.getRut());
             ins.setString(3, m.getNombre());
@@ -27,11 +31,99 @@ public class ConsultasModelo {
             if(ins.executeUpdate()>0){
                 return true;
             }
+            
             c.close(ins);
         } catch (Exception e) {
             System.out.println("Error al agregar " + e.getMessage());
         }
         return false;
+    }
+    
+    public boolean eliminarEmpleadoporCodigo(int codigo) {
+        try {
+            String query = "DELETE FROM contrata.empleados WHERE codigo=?";
+            PreparedStatement eli = c.getConexion().prepareStatement(query);
+            
+            eli.setInt(1, codigo);
+
+            if (eli.executeUpdate() > 0) {
+                return true;
+            }
+            
+            c.close(eli);
+        } catch (Exception e) {
+            System.out.println("Error al eliminar " + e.getMessage());
+        }
+        return false;
+    }
+    
+    public boolean modificarEmpleadoporCodigo(Modelo m) {
+        try {
+            String query = "UPDATE contrata.empleados SET rut=?, nombre=?, apellido=?, celular=?, email=?, sueldo_bruto=?, est_civil=?, nom_depto=? WHERE codigo=?";
+            PreparedStatement mod = c.getConexion().prepareStatement(query);
+           
+            mod.setString(1, m.getRut());
+            mod.setString(2, m.getNombre());
+            mod.setString(3, m.getApellido());
+            mod.setInt(4, m.getCelular());
+            mod.setString(5, m.getEmail());
+            mod.setDouble(6, m.getSueldo_bruto());
+            mod.setString(7, m.getEst_civil());
+            mod.setString(8, m.getNom_depto());
+            mod.setInt(9, m.getCodigo());
+
+            if (mod.executeUpdate() > 0) {
+                return true;
+            }
+            
+            c.close(mod);
+        } catch (Exception e) {
+            System.out.println("Error al modificar " + e.getMessage());
+        }
+        return false;
+    }
+    
+    public DefaultTableModel MostrarEmpleados() throws ClassNotFoundException, SQLException {
+        
+        DefaultTableModel tablemodel = new DefaultTableModel();
+        int registros = 0;
+        String[] columNames = {"CODIGO", "RUT", "NOMBRE", "APELLIDO", "CELULAR", "EMAIL", "SUELDO BRUTO", "EST CIVIL", "NOMBRE DEPTO"};
+        
+        try {
+            PreparedStatement pstm = c.getConexion().prepareStatement("SELECT count(*) as total FROM empleados");
+            ResultSet res = pstm.executeQuery();
+            res.next();
+            registros = res.getInt("total");
+            res.close();
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        
+        Object[][] data = new String[registros][9];
+        try {
+            PreparedStatement pstm = c.getConexion().prepareStatement("SELECT codigo, rut, nombre, apellido, celular, email, sueldo_bruto, est_civil, nom_depto FROM contrata.empleados order by codigo ASC");
+            ResultSet res = pstm.executeQuery();
+            
+            int i = 0;
+            while (res.next()) {
+                data[i][0] = res.getString("codigo");
+                data[i][1] = res.getString("rut");
+                data[i][2] = res.getString("nombre");
+                data[i][3] = res.getString("apellido");
+                data[i][4] = res.getString("celular");
+                data[i][5] = res.getString("email");
+                data[i][6] = res.getString("sueldo_bruto");
+                data[i][7] = res.getString("est_civil");
+                data[i][8] = res.getString("nom_depto");
+                i++;
+            }
+            
+            res.close();
+            tablemodel.setDataVector(data, columNames);
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        return tablemodel;
     }
     
 }
